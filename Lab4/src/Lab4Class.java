54a189e177f2e6a23677e92b68c401963a5bc0d9
kidcordie <acordero1@ymail.com>
2014-09-17 16:22:37 -0600

    Removed a comment

--- Lab4/src/Lab4Class.java
+++ Lab4/src/Lab4Class.java
@@ -4,7 +4,6 @@ public class Lab4Class {
 	public static void main(String[] args) {
 		System.out.println("Hello World"); // Hope
 		System.out.println("How are you?"); // Good
-		// This is just a comment
 	}
 
 }
